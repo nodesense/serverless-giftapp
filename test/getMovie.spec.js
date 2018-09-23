745d10bbc2184ca778e9dbbf5388b62b1adadaac
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-09-23 19:15:52 +0530

    getMovie and tests sample

--- test/getMovie.spec.js
+++ test/getMovie.spec.js
@@ -1,6 +1,11 @@
 const getMovie = require('../src/movies/getMovie');
 
-getMovie.handler({}, {}, function(err, result) {
-    console.log('Error ', err);
-    console.log('Result ', result);
+describe("getMovie test suites", () => {
+    it("getMovie", (done) => {
+        getMovie.handler({}, {}, function(err, result) {
+            console.log('Error ', err);
+            console.log('Result ', result);
+            done()
+        })
+    })
 })
